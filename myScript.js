abfe54d1726d875a5e8ffb878701670d22e4f41d
Annii <anish.1903.kmr@gmail.com>
2021-07-12 19:43:43 +0530

    js

--- myScript.js
+++ myScript.js
@@ -2,3 +2,18 @@ console.log("Namaste JavaScript")
 
 var city = 'berlin'; // name of an variable is called an identifier in javascript
 
+// rules for variable names
+// we can start variable name with '_', '$', or alphabets
+var _javascript;
+var $name;
+var firstName;
+
+// comments in js
+/* 
+
+This is single line comment --> // put your comments comments
+
+multiline comments --> this whole thing is a multiline comment in itself
+
+*/
+
