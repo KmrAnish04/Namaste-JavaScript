5063405e5e3e3600d7c225700faa1399bfed1992
Annii <anish.1903.kmr@gmail.com>
2021-10-20 21:09:42 +0530

    Project Completed

--- My_First_JavaScript_Project/static/js/script.js
+++ My_First_JavaScript_Project/static/js/script.js
@@ -1,1 +1,16 @@
-alert("hello");
+
+function inputAge(){
+    var birthYear = prompt("Enter your Birth Year...");
+    var ageInDays = (2021 - birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var resultText = document.createTextNode('You are' + ' ' + ageInDays + ' ' + 'days old.');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(resultText);
+    document.getElementById('flexbox-result').appendChild(h1);
+
+    console.log(ageInDays);
+}
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
