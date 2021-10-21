5eaad3392b0714a31d95b78daae5eba35d7310db
Annii <anish.1903.kmr@gmail.com>
2021-10-22 01:01:49 +0530

    done with bot's choice

--- My_First_JavaScript_Project_2.O/static/js/script.js
+++ My_First_JavaScript_Project_2.O/static/js/script.js
@@ -1,3 +1,16 @@
 function rpsGame(userChoice){
-    console.log(userChoice);
+    var playerChoice, botChoice;
+
+    botChoice = numToBotChoice(randToBotChoice())
+    console.log(botChoice);
+
+}
+
+function randToBotChoice() {
+    return Math.floor(Math.random() * 3);
+}
+
+function numToBotChoice(num) {
+    var botChoices = ['rock', 'paper', 'scissor'];
+    return botChoices[num]; 
 }
